2a66edf4f7551d982a801c3bda008594b9e09973
mesut <112901255+mesutgdk@users.noreply.github.com>
2022-12-26 12:33:47 -0800

    Add files via upload

--- Todoey/Controllers/CategoryViewControllerTableViewController.swift
+++ Todoey/Controllers/CategoryViewControllerTableViewController.swift
@@ -2,16 +2,13 @@
 //  CategoryViewControllerTableViewController.swift
 //  Todoey
 //
-//  Created by mesut on 24.12.2022.
-//  Copyright © 2022 App Brewery. All rights reserved.
-//
 
 import UIKit
 import CoreData
 
 class CategoryViewControllerTableViewController: UITableViewController {
 
-    var categoryArray = [Category]()
+    var categories = [Category]()
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -27,11 +24,11 @@ class CategoryViewControllerTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return categoryArray.count
+        return categories.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        cell.textLabel?.text = categoryArray[indexPath.row].name
+        cell.textLabel?.text = categories[indexPath.row].name
         return cell
     }
     
@@ -41,7 +38,7 @@ class CategoryViewControllerTableViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
             let newCategory = Category(context: self.context)
             newCategory.name = textField.text!
-            self.categoryArray.append(newCategory)
+            self.categories.append(newCategory)
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create a New Category"
@@ -61,7 +58,7 @@ class CategoryViewControllerTableViewController: UITableViewController {
     func loadData () {
         let request: NSFetchRequest <Category> = Category.fetchRequest()
         do {
-            categoryArray = try context.fetch(request)
+            categories = try context.fetch(request)
         } catch {
             print("Error fetching data from context, \(error)")
         }
@@ -71,7 +68,7 @@ class CategoryViewControllerTableViewController: UITableViewController {
 }
 extension CategoryViewControllerTableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(categoryArray[indexPath.row].name ?? "none")
+        print(categories[indexPath.row].name ?? "none")
     }
 }
 
